2e48580741826230fab108af251ce44fe7534395
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-03 21:12:25 +0100

    set fallback values

--- src/item-price/item-price.service.ts
+++ src/item-price/item-price.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { QueryResult } from 'pg';
 import { client } from 'src/database.service';
-import { ItemPrice } from 'src/graphql.schema';
+import { ItemPrice, ItemPriceVisibility } from 'src/graphql.schema';
 import { CreateItemPriceDto } from './dto/create-item-price.dto';
 
 @Injectable()
 export class ItemPriceService {
 	public async create(itemPrice: CreateItemPriceDto): Promise<ItemPrice> {
+		if (!itemPrice.scanTime) {
+			itemPrice.scanTime = new Date();
+		}
+		if (itemPrice.visibility === undefined) {
+			itemPrice.visibility = ItemPriceVisibility.PUBLIC;
+		}
 		const result: QueryResult = await client.query(
 			'INSERT INTO item_price(scanned_by_id, item_id, location_id, price, quantity, scan_time, type, visibility)' +
 				' VALUES ($1::uuid, $2::uuid, $3::uuid, $4::numeric,' +
