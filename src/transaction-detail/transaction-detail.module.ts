feb630cd27b1bfc10134d19fca3b157573435d57
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-05 20:38:00 +0100

    added forwardRef

--- src/transaction-detail/transaction-detail.module.ts
+++ src/transaction-detail/transaction-detail.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TransactionModule } from 'src/transaction/transaction.module';
 import { TransactionService } from 'src/transaction/transaction.service';
 import { TransactionDetailResolvers } from './transaction-detail.resolvers';
 import { TransactionDetailService } from './transaction-detail.service';
 
 @Module({
-	imports: [TransactionModule],
+	imports: [forwardRef(() => TransactionModule)],
 	providers: [TransactionDetailService, TransactionDetailResolvers, TransactionService]
 })
 export class TransactionDetailModule {}
--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TransactionDetailModule } from 'src/transaction-detail/transaction-detail.module';
 import { TransactionDetailService } from 'src/transaction-detail/transaction-detail.service';
 import { TransactionResolvers } from './transaction.resolvers';
 import { TransactionService } from './transaction.service';
 
 @Module({
-	imports: [TransactionDetailModule],
+	imports: [forwardRef(() => TransactionDetailModule)],
 	providers: [TransactionService, TransactionResolvers, TransactionDetailService]
 })
 export class TransactionModule {}
