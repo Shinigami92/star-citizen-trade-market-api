6c8ab1689b87d6dd85b8685b6ae656145924111c
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-08 10:10:08 +0100

    configurable api port

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { INestApplication, INestExpressApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { client } from './database.service';
+
+dotenv.config();
+
+export const PORT: number = process.env.API_PORT !== undefined ? +process.env.API_PORT! : 3000;
 
 async function bootstrap(): Promise<void> {
 	const app: INestApplication & INestExpressApplication = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
-	const port: number = 3000;
-	await app.listen(port);
-	Logger.log(`Started at http://localhost:${port}`, 'bootstrap');
-	Logger.log(`GraphQL endpoint http://localhost:${port}/graphql`, 'bootstrap');
+	await app.listen(PORT);
+	Logger.log(`Started at http://localhost:${PORT}`, 'bootstrap');
+	Logger.log(`GraphQL endpoint http://localhost:${PORT}/graphql`, 'bootstrap');
 
 	await client.connect();
 	Logger.log('Connected to database', 'bootstrap');
