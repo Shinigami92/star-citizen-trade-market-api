1afec9d946da917042807f87cda449bfc7c43bc5
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-09 12:12:47 +0100

    added items resolver

--- src/item/item.resolvers.ts
+++ src/item/item.resolvers.ts
@@ -1,7 +1,16 @@
-import { Parent, ResolveProperty, Resolver } from '@nestjs/graphql';
+import { Parent, Query, ResolveProperty, Resolver } from '@nestjs/graphql';
+import { Item } from 'src/graphql.schema';
+import { ItemService } from './item.service';
 
 @Resolver('Item')
 export class ItemResolvers {
+	constructor(private readonly itemService: ItemService) {}
+
+	@Query('items')
+	public async items(): Promise<Item[]> {
+		return await this.itemService.findAll();
+	}
+
 	@ResolveProperty('__resolveType')
 	public async resolveType(@Parent() item: any): Promise<string | undefined> {
 		switch (item.type) {
