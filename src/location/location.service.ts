a2de81741c4a05c357bffa8ff3e0693a1e633e07
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-20 11:30:29 +0100

    refactored LocationService

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,33 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { QueryResult } from 'pg';
 import { client } from 'src/database.service';
 import { Location } from 'src/graphql.schema';
 import { CreateLocationDto } from './dto/create-location.dto';
 
+export const TABLENAME: string = 'location';
+
 @Injectable()
 export class LocationService {
-	public async create(location: CreateLocationDto): Promise<Location> {
+	private readonly logger: Logger = new Logger(LocationService.name);
+
+	public async create({
+		name,
+		parentLocationId,
+		typeId,
+		inGameSinceVersionId,
+		inGameSince
+	}: CreateLocationDto): Promise<Location> {
 		const result: QueryResult = await client.query(
-			'INSERT INTO location(name, parent_location_id, type_id, in_game_since_version_id, in_game_since)' +
+			`INSERT INTO ${TABLENAME}(name, parent_location_id, type_id, in_game_since_version_id, in_game_since)` +
 				' VALUES ($1::text, $2::uuid, $3::uuid, $4::uuid, $5::timestamptz) RETURNING *',
-			[
-				location.name,
-				location.parentLocationId,
-				location.typeId,
-				location.inGameSinceVersionId,
-				location.inGameSince
-			]
+			[name, parentLocationId, typeId, inGameSinceVersionId, inGameSince]
 		);
-		return result.rows[0];
+		const created: Location = result.rows[0];
+		this.logger.log(`Created ${TABLENAME} with id ${created.id}`);
+		return created;
 	}
 
 	public async findAll(): Promise<Location[]> {
-		const result: QueryResult = await client.query('SELECT * FROM location ORDER BY name');
+		const result: QueryResult = await client.query(`SELECT * FROM ${TABLENAME} ORDER BY name`);
 		return result.rows;
 	}
 
 	public async findOneById(id: string): Promise<Location | undefined> {
-		const result: QueryResult = await client.query('SELECT * FROM location WHERE id = $1::uuid', [id]);
+		const result: QueryResult = await client.query(`SELECT * FROM ${TABLENAME} WHERE id = $1::uuid`, [id]);
 		return result.rows[0];
 	}
 }
