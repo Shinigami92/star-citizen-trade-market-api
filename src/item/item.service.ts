3c82d9cc2683f111188087fe0f702d8ed50fb178
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-20 10:53:32 +0100

    refactored ItemService

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -3,15 +3,17 @@ import { QueryResult } from 'pg';
 import { client } from 'src/database.service';
 import { Item } from 'src/graphql.schema';
 
+export const TABLENAME: string = 'item';
+
 @Injectable()
 export class ItemService {
 	public async findAll(): Promise<Item[]> {
-		const result: QueryResult = await client.query('SELECT * FROM item ORDER BY name');
+		const result: QueryResult = await client.query(`SELECT * FROM ${TABLENAME} ORDER BY name`);
 		return result.rows;
 	}
 
 	public async findOneById(id: string): Promise<Item | undefined> {
-		const result: QueryResult = await client.query('SELECT * FROM item WHERE id = $1::uuid', [id]);
+		const result: QueryResult = await client.query(`SELECT * FROM ${TABLENAME} WHERE id = $1::uuid`, [id]);
 		return result.rows[0];
 	}
 }
