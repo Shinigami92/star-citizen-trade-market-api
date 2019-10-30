cd1f66b0c57f0cc9e40825e8f2cb4f59ff19ff30
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2019-10-30 15:16:13 +0100

    Extract class into own file

---
+++ src/transaction/dto/create-first-transaction-detail.dto.ts
@@ -0,0 +1,19 @@
+import { IsDate, IsInt, IsNumber, IsOptional, IsPositive, IsString, IsUUID } from 'class-validator';
+import { CreateFirstTransactionDetailInput } from 'src/graphql.schema';
+
+export class CreateFirstTransactionDetailDto implements CreateFirstTransactionDetailInput {
+	@IsUUID('4')
+	public locationId!: string;
+	@IsNumber()
+	@IsPositive()
+	public price!: number;
+	@IsInt()
+	@IsPositive()
+	public quantity!: number;
+	@IsOptional()
+	@IsString()
+	public note?: string | undefined;
+	@IsOptional()
+	@IsDate()
+	public timestamp?: Date;
+}
--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,22 +1,6 @@
-import { IsDate, IsInt, IsNumber, IsOptional, IsPositive, IsString, IsUUID } from 'class-validator';
-import { CreateFirstTransactionDetailInput, CreateTransactionInput } from 'src/graphql.schema';
-
-export class CreateFirstTransactionDetailDto implements CreateFirstTransactionDetailInput {
-	@IsUUID('4')
-	public locationId!: string;
-	@IsNumber()
-	@IsPositive()
-	public price!: number;
-	@IsInt()
-	@IsPositive()
-	public quantity!: number;
-	@IsOptional()
-	@IsString()
-	public note?: string | undefined;
-	@IsOptional()
-	@IsDate()
-	public timestamp?: Date;
-}
+import { IsOptional, IsUUID } from 'class-validator';
+import { CreateTransactionInput } from 'src/graphql.schema';
+import { CreateFirstTransactionDetailDto } from './create-first-transaction-detail.dto';
 
 export class CreateTransactionDto implements CreateTransactionInput {
 	@IsOptional()
