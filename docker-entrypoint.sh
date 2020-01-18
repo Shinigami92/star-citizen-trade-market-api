97cfcfe2c3241150d9e0333d249191316e5dc3d3
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-18 15:08:47 +0100

    Log wait message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -xe
 
-echo Hello World
-
+echo "Wait 10 seconds for database container"
 sleep 10
-DATABASE_URL=postgres://postgres:postgres@sctm-database:5433/sctm yarn migrate up
+
+DATABASE_URL=postgres://postgres:postgres@sctm-database:5432/sctm yarn migrate up
 
 yarn start:prod
