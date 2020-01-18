8fb89471a62220f23a803b7fef0f1e9bdaaf2644
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-18 10:55:26 +0100

    Configurable hostname

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,20 @@ import { client } from './database.service';
 
 dotenv.config();
 
-export const PORT: number = process.env.API_PORT !== undefined ? +process.env.API_PORT! : 3000;
+const HOSTNAME: string | undefined = process.env.API_HOSTNAME;
+const PORT: number = process.env.API_PORT !== undefined ? +process.env.API_PORT : 3000;
 
 async function bootstrap(): Promise<void> {
 	const app: INestApplication = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(PORT);
-	Logger.log(`Started at http://localhost:${PORT}`, 'bootstrap');
-	Logger.log(`GraphQL endpoint http://localhost:${PORT}/graphql`, 'bootstrap');
+	if (HOSTNAME) {
+		await app.listen(PORT, HOSTNAME);
+	} else {
+		await app.listen(PORT);
+	}
+	const url: string = await app.getUrl();
+	Logger.log(`Started at ${url}`, 'bootstrap');
+	Logger.log(`GraphQL endpoint ${url}/graphql`, 'bootstrap');
 
 	await client.connect();
 	Logger.log('Connected to database', 'bootstrap');
