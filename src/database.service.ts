5deee727bbfd5f02f108092fa194c36c55f6be2e
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-18 15:45:11 +0100

    Prefer env DATABASE_URL

--- src/database.service.ts
+++ src/database.service.ts
@@ -9,10 +9,20 @@ pgCamelCase.inject(pg);
 
 dotenv.config();
 
-export const client: Client = new Client({
-	host: process.env.DB_HOST,
-	port: +process.env.DB_PORT!,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
-});
+let client: Client;
+
+const DATABASE_URL: string | undefined = process.env.DATABASE_URL;
+
+if (DATABASE_URL === undefined) {
+	client = new Client({
+		host: process.env.DB_HOST,
+		port: +process.env.DB_PORT!,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME
+	});
+} else {
+	client = new Client(DATABASE_URL);
+}
+
+export { client };
